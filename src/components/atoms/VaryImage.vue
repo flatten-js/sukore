c55da5000ae344bddf0b07b1971df3c3c68db753
Saito11022 <saito11022@gmail.com>
2019-10-08 03:09:19 +0900

    Add: create atoms to VaryImage

---
+++ src/components/atoms/VaryImage.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  img.vary-image(
+    :class="fitClassGeneration"
+    :src="src"
+    )
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    fit: {
+      type: [String, null],
+      default: null,
+      varidator(val) {
+        return ['cover', null].includes(val)
+      }
+    }
+  },
+  computed: {
+    fitClassGeneration() {
+      const { fit } = this
+      return fit ? `fit-${fit}` : null
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .vary-image {
+    width: 100%;
+    height: 100%;
+
+    &.fit-cover {
+      object-fit: cover;
+    }
+  }
+</style>
