fff6053b5c752c1e2ec955b898bf8a150dc8c1af
Saito11022 <saito11022@gmail.com>
2019-10-08 04:12:46 +0900

    Add: create molecules to ThumbnailBox

---
+++ src/components/molecules/ThumbnailBox.vue
@@ -0,0 +1,70 @@
+<template lang="pug">
+  div.thumbnail-box(:class="columnClassGeneration")
+    vary-image(
+      :src="src"
+      fit="cover"
+      )
+    .thumbnail-fav
+      material-icons(
+        name="favorite"
+        size="custom"
+        )
+</template>
+
+<script>
+import VaryImage from '@/components/atoms/VaryImage.vue'
+import MaterialIcons from '@/components/atoms/MaterialIcons.vue'
+
+export default {
+  components: {
+    VaryImage,
+    MaterialIcons
+  },
+  props: {
+    src: {
+      type: String,
+      reqired: true
+    },
+    column: {
+      type: String,
+      default: '2',
+      varidator(val) {
+        return ['2', '3'].includes(val)
+      }
+    }
+  },
+  computed: {
+    columnClassGeneration() {
+      const { column } = this
+      return `column-${column}`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .thumbnail-box {
+    position: relative;
+
+    &.column-2 {
+      width: 50vw;
+      height: 50vw;
+    }
+
+    &.column-3 {
+      width: 33.33vw;
+      height: 33.33vw;
+    }
+  }
+
+  .thumbnail-fav {
+    position: absolute;
+    padding: .5rem;
+    bottom: 0;
+    right: 0;
+    color: rgba(255, 255, 255, .95);
+    font-size: 1.8rem;
+    line-height: 0;
+    text-shadow: 0 0 3px rgba(26, 26, 26, .2);
+  }
+</style>
