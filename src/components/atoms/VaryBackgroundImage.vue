9cf8df02d56d1330e38b205596bf1368a7f8cbf8
Saito11022 <saito11022@gmail.com>
2019-10-08 04:53:24 +0900

    Add: create atoms to VaryBackgroundImage

---
+++ src/components/atoms/VaryBackgroundImage.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  .vary-background-image(
+    :class="attachmentClassGeneration"
+    :style="{ backgroundImage: `url(${url})` }"
+    )
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true
+    },
+    attachment: {
+      type: [String, null],
+      default: null,
+      varidator(val) {
+        return ['fixed'].includes(val)
+      }
+    }
+  },
+  computed: {
+    attachmentClassGeneration() {
+      const { attachment } = this
+      return attachment ? `background-attachment-${attachment}` : null
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .vary-background-image {
+    width: 100%;
+    height: 100%;
+
+    &.background-attachment-fixed {
+      background-attachment: fixed;
+    }
+  }
+</style>
