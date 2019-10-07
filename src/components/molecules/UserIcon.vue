e5bb85d0d123ad42137601ede9bc8a1eb019b76e
Saito11022 <saito11022@gmail.com>
2019-10-08 04:36:08 +0900

    Add: create molecules to UserIcon

---
+++ src/components/molecules/UserIcon.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  .user-icon
+    vary-image(
+      :src="src"
+      fit="cover"
+      )
+</template>
+
+<script>
+import VaryImage from '@/components/atoms/VaryImage.vue'
+
+export default {
+  components: {
+    VaryImage
+  },
+  props: {
+    src: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .user-icon {
+    width: 70px;
+    height: 70px;
+    border: 3px solid #fff;
+    border-radius: 50%;
+    box-sizing: border-box;
+  }
+</style>
