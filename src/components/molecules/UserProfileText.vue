db33f07117c4ac63e5a4edc5fa9aecd97b4ea509
Saito11022 <saito11022@gmail.com>
2019-10-08 23:09:11 +0900

    Add: create molecules to UserProfileText

---
+++ src/components/molecules/UserProfileText.vue
@@ -0,0 +1,82 @@
+<template lang="pug">
+  .user-profile-text
+    .user-name
+      block-heading(:text="name")
+      block-text(
+        :text="screenName"
+        brightness="2"
+        )
+    .user-descripstion
+      block-text(
+        :text="descripstion"
+        )
+    .user-follow-status
+      .user-follow
+        inline-part(
+          :part="follow"
+          weight="bold"
+          mr="05"
+          )
+        inline-part(
+          part="フォロー中"
+          weight="normal"
+          )
+      .user-followers
+        inline-part(
+          :part="followers"
+          weight="bold"
+          mr="05"
+          )
+        inline-part(
+          part="フォロワー"
+          weight="normal"
+          )
+</template>
+
+<script>
+import BlockHeading from '@/components/atoms/BlockHeading.vue'
+import BlockText from '@/components/atoms/BlockText.vue'
+import InlinePart from '@/components/atoms/InlinePart.vue'
+
+export default {
+  components: {
+    BlockHeading,
+    BlockText,
+    InlinePart
+  },
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    screenName: {
+      type: String,
+      required: true
+    },
+    descripstion: {
+      type: String,
+      required: true
+    },
+    follow: {
+      type: String,
+      required: true
+    },
+    followers: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .user-name, .user-descripstion {
+    margin: 0 0 1rem 0;
+  }
+
+  .user-follow-status {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+  }
+</style>
