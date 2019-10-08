0eee2bad39a36f270cc9156e5e3a17a5f6d57e6a
Saito11022 <saito11022@gmail.com>
2019-10-09 00:21:40 +0900

    Add: create organisms to UserProfile

---
+++ src/components/organisms/UserProfile.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .user-profile
+    .user-header-image
+      slot(name="header-image")
+    .user-profile-body
+      .user-profile-icon
+        slot(name="user-icon")
+      slot(name="user-profile-text")
+</template>
+
+<style scoped>
+  .user-profile-body {
+    padding: 0 1rem 1rem 1rem;
+    border-bottom: 1px solid rgba(26, 26, 26, .1);
+    box-sizing: border-box;
+  }
+
+  .user-profile-icon {
+    display: inline-block;
+    margin: -35px 0 1rem;
+  }
+</style>
