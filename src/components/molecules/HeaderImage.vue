7140b74f3cebfa0b349d8974cef1613e2b1449ca
Saito11022 <saito11022@gmail.com>
2019-10-08 12:53:15 +0900

    Add: create molecules to HeaderImage

---
+++ src/components/molecules/HeaderImage.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  .header-image
+    vary-background-image(
+      :url="url"
+      attachment="fixed"
+      size="contain"
+      )
+</template>
+
+<script>
+import VaryBackgroundImage from '@/components/atoms/VaryBackgroundImage.vue'
+
+export default {
+  components: {
+    VaryBackgroundImage
+  },
+  props: {
+    url: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .header-image {
+    width: 100%;
+    height: 125px;
+  }
+</style>
