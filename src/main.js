838f1698614dd45802bc3371e8211073a79d7376
Saito11022 <saito11022@gmail.com>
2019-10-08 23:07:38 +0900

    Add: sukore project of adobe fonts

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 import 'normalize.css'
+import '@/assets/css/root.css'
 
 Vue.config.productionTip = false
 
