7d4fbedd501bb754826bd2d363fcf2938fbd8540
Saito11022 <saito11022@gmail.com>
2019-10-08 21:12:18 +0900

    Add: create atoms to BlockHeading

---
+++ src/components/atoms/BlockHeading.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  .block-heading {{ text }}
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .block-heading {
+    font-size: 1.125rem;
+    font-weight: bold;
+  }
+</style>
