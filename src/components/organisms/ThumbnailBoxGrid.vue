5879ba04b844be49e2b303dcbe5c09a10f4f0b3e
Saito11022 <saito11022@gmail.com>
2019-10-09 00:36:43 +0900

    Add: create organisms to ThumbnailBoxGrid

---
+++ src/components/organisms/ThumbnailBoxGrid.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  .thumbnail-box-grid
+    slot
+</template>
+
+<style scoped>
+  .thumbnail-box-grid {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+  }
+</style>
