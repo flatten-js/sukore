afc4fb0e4fef4cc094cf4f5dfe266a7a4720faea
Setting6 <thetakahito@gmail.com>
2019-10-12 23:51:46 +0900

    Add: create views in children to Tweet

---
+++ src/components/views/Pickup/children/Tweet.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  thumbnail-box-grid
+    template(v-for="media in tweetMedia")
+      thumbnail-box(
+        :key="media.id"
+        :src="media.src"
+        )
+</template>
+
+<script>
+import ThumbnailBoxGrid from '@/components/organisms/ThumbnailBoxGrid.vue'
+import ThumbnailBox from '@/components/molecules/ThumbnailBox.vue'
+
+export default  {
+  components: {
+    ThumbnailBoxGrid,
+    ThumbnailBox
+  },
+  computed: {
+    tweetMedia() {
+      return this.$store.getters.tweetFilter
+    }
+  }
+}
+</script>
