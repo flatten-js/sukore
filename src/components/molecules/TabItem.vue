63a2204e910d359f6fb228b9b548a469faac3d61
Setting6 <thetakahito@gmail.com>
2019-10-12 20:37:41 +0900

    Add: create molecules to TabItem

---
+++ src/components/molecules/TabItem.vue
@@ -0,0 +1,61 @@
+<template lang="pug">
+  .tab-item(:class="propsClassGenerator")
+    v-router-link(
+      :to="to"
+      :text="text"
+      size="small"
+      p="1"
+      )
+</template>
+
+<script>
+import VRouterLink from '@/components/atoms/VRouterLink.vue'
+
+export default {
+  components: {
+    VRouterLink
+  },
+  props: {
+    to: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      default: 'default',
+      validator(val) {
+        return ['default'].includes(val)
+      }
+    }
+  },
+  computed: {
+    propsClassGenerator() {
+      const { color } = this
+
+      return {
+        [`color-${color}`]: color
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .tab-item {
+    display: inline-block;
+    border-bottom: 2px solid rgba(26, 26, 26, 0);
+
+    &.color-default {
+      color: rgba(26, 26, 26, .5);
+
+      & > .router-link-exact-active {
+        color: rgba(26, 26, 26, 1);
+        border-bottom: 2px solid #1a1a1a;
+      }
+    }
+  }
+</style>
