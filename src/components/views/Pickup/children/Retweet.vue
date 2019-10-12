d9db4473506464acc4b66393fc5a017908e6ce20
Setting6 <thetakahito@gmail.com>
2019-10-12 23:52:57 +0900

    Add: create views in children to Retweet

---
+++ src/components/views/Pickup/children/Retweet.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  thumbnail-box-grid
+    template(v-for="media in retweetMedia")
+      thumbnail-box(
+        :key="media.id"
+        :src="media.src"
+        )
+</template>
+
+<script>
+import ThumbnailBoxGrid from '@/components/organisms/ThumbnailBoxGrid.vue'
+import ThumbnailBox from '@/components/molecules/ThumbnailBox.vue'
+
+export default  {
+  components: {
+    ThumbnailBoxGrid,
+    ThumbnailBox
+  },
+  computed: {
+    retweetMedia() {
+      return this.$store.getters.retweetFilter
+    }
+  }
+}
+</script>
