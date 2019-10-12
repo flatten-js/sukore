5a3a736f5380153296e5c06122978496c02d3550
Setting6 <thetakahito@gmail.com>
2019-10-12 21:00:30 +0900

    Add: nav-tabs-section

--- src/components/templates/PickupTemplate.vue
+++ src/components/templates/PickupTemplate.vue
@@ -2,6 +2,8 @@
   .pickup-template
     section.user-detail-section
       slot(name="user-detail")
+    section.nav-tabs-section
+      slot(name="nav-tabs")
     section.thumbnail-box-section
       slot(name="thumbnail-box")
 </template>
