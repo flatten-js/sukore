74ce99778cba0e99df599201373a20f6386d7ad4
Setting6 <thetakahito@gmail.com>
2019-10-20 00:57:35 +0900

    Add: create organisms to UserDetailBar

--- src/components/views/PickupDetail/index.vue
+++ src/components/views/PickupDetail/index.vue
@@ -1,13 +1,17 @@
 <template lang="pug">
   pickup-detail-template
+    template(v-slot:user-detail-bar)
+      user-detail-bar
 </template>
 
 <script>
 import PickupDetailTemplate from '@/components/templates/PickupDetailTemplate.vue'
+import UserDetailBar from '@/components/organisms/UserDetailBar.vue'
 
 export default {
   components: {
-    PickupDetailTemplate
+    PickupDetailTemplate,
+    UserDetailBar
   }
 }
 </script>
