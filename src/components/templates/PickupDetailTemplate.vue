fed6bd900382e0c4438bdbef45f6c271596a4346
Setting6 <thetakahito@gmail.com>
2019-10-19 23:46:08 +0900

    Add: create Templates to PickupDetailTemplate

---
+++ src/components/templates/PickupDetailTemplate.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  .pickup-detail-template
+    section.user-detail-bar-section
+      slot(name="user-detail-bar")
+    section.thumbnail-detail-section
+      slot(name="thumbnail-detail")
+</template>
+
+<style scoped>
+  .thumbnail-detail-section {
+    border-bottom: 1px solid rgba(26, 26, 26, .2);
+  }
+</style>
--- src/components/views/PickupDetail/index.vue
+++ src/components/views/PickupDetail/index.vue
@@ -1,2 +1,13 @@
 <template lang="pug">
+  pickup-detail-template
 </template>
+
+<script>
+import PickupDetailTemplate from '@/components/templates/PickupDetailTemplate.vue'
+
+export default {
+  components: {
+    PickupDetailTemplate
+  }
+}
+</script>
