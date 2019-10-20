65ece12c90b010e301b7de04216e815f6ea37649
Setting6 <thetakahito@gmail.com>
2019-10-20 18:48:43 +0900

    Add: create molecules to OutsideLinkButton

---
+++ src/components/molecules/OutsideLinkButton.vue
@@ -0,0 +1,62 @@
+<template lang="pug">
+  .outside-link-button(:class="propsClassGenerator")
+    outside-link(
+      :text="text"
+      :href="href"
+      :size="size"
+      )
+</template>
+
+<script>
+import OutsideLink from '@/components/atoms/OutsideLink.vue'
+
+export default {
+  components: {
+    OutsideLink
+  },
+  props: {
+    text: {
+      type: String,
+      reqired: true
+    },
+    href: {
+      type: String,
+      reqired: true
+    },
+    type: {
+      type: String,
+      default: 'default',
+      validator(val) {
+        return ['default', 'twitter'].includes(val)
+      }
+    },
+    size: String
+  },
+  computed: {
+    propsClassGenerator() {
+      const { type } = this
+
+      return {
+        [`type-${type}`]: type
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .outside-link-button {
+    padding: .35rem 1rem;
+    border-radius: 25px;
+
+    &.type-default {
+      background-color: #1a1a1a;
+      color: white;
+    }
+
+    &.type-twitter {
+      background-color: #1DA1F2;
+      color: white;
+    }
+  }
+</style>
