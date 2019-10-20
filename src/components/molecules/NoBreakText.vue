7e2314b8e13f2377374f92070ad569172ee02786
Setting6 <thetakahito@gmail.com>
2019-10-20 17:34:06 +0900

    Add: create molecules to NoBreakText

---
+++ src/components/molecules/NoBreakText.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  .no-break-text
+    block-text(
+      :text="text"
+      :size="size"
+      :weight="weight"
+      )
+</template>
+
+<script>
+import BlockText from '@/components/atoms/BlockText.vue'
+
+export default {
+  components: {
+    BlockText
+  },
+  props: {
+    text: {
+      type: String,
+      reqired: true
+    },
+    size: String,
+    weight: String
+  }
+}
+</script>
+
+<style scoped>
+  .no-break-text > .block-text {
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+</style>
