e0054be3b391d0d3f77a9fbfa3b3b74ae03980fe
Setting6 <thetakahito@gmail.com>
2019-10-27 22:20:37 +0900

    Add: create Pickup children to RetweetTabContent

---
+++ src/components/views/Pickup/children/RetweetTabContent.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  thumbnail-box-grid
+    template(v-for="media in retweetFilter")
+      thumbnail-box(
+        :key="media.id"
+        :id="media.id"
+        :src="media.src[0]"
+        :size="media.size"
+        animation="fade"
+        )
+</template>
+
+<script>
+import ThumbnailBoxGrid from '@/components/organisms/ThumbnailBoxGrid.vue'
+import ThumbnailBox from '@/components/molecules/ThumbnailBox.vue'
+
+export default {
+  components: {
+    ThumbnailBoxGrid,
+    ThumbnailBox
+  },
+  computed: {
+    retweetFilter() {
+      return this.$store.getters.retweetFilter
+    }
+  }
+}
+</script>
