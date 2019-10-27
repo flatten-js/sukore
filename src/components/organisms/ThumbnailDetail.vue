f1bf735b33894c349a1e7ecaad5b9f244581b1a8
Setting6 <thetakahito@gmail.com>
2019-10-27 11:31:35 +0900

    Improve: Renamed ThumbnailFigureg from molecules to organisms

--- src/components/organisms/ThumbnailDetail.vue
+++ src/components/organisms/ThumbnailDetail.vue
@@ -1,4 +1,32 @@
 <template lang="pug">
-  .thumbnail-detail
-    slot
+  figure.thumbnail-detail
+    .thumbnail-area
+      slot(name="thumbnail")
+      .thumbnail-more
+        slot(name="more")
+    figcaption.thumbnail-detail-caption
+      slot(name="caption")
 </template>
+
+<style scoped>
+  .thumbnail-detail {
+    width: 100%;
+    margin: 0;
+  }
+
+  .thumbnail-area {
+    position: relative;
+  }
+
+  .thumbnail-more {
+    position: absolute;
+    bottom: 1rem;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .thumbnail-detail-caption {
+    padding: 1rem;
+    box-sizing: border-box;
+  }
+</style>
