843ff82ec5993dcc45bb1d3bf781061783fd6fb7
Setting6 <thetakahito@gmail.com>
2019-10-27 22:19:59 +0900

    Add: create Pickup children to TweetTabContent

---
+++ src/components/views/Pickup/children/TweetTabContent.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  thumbnail-box-grid
+    template(v-for="media in tweetFilter")
+      thumbnail-box(
+        :key="media.id"
+        :id="media.id"
+        :src="media.src[0]"
+        :size="media.size"
+        animation="fade"
+        )
+</template>
+
+<script>
+import ThumbnailBoxGrid from '@/components/organisms/ThumbnailBoxGrid.vue'
+import ThumbnailBox from '@/components/molecules/ThumbnailBox.vue'
+
+export default {
+  components: {
+    ThumbnailBoxGrid,
+    ThumbnailBox
+  },
+  computed: {
+    tweetFilter() {
+      return this.$store.getters.tweetFilter
+    }
+  }
+}
+</script>
