efd8b51c184bbd6f49216465684bcfa6401cd632
Setting6 <thetakahito@gmail.com>
2019-10-27 10:53:02 +0900

    Add: create molecules to Thumbnail

---
+++ src/components/molecules/Thumbnail.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  .thumbnail
+    vary-image(:src="src")
+</template>
+
+<script>
+import VaryImage from '@/components/atoms/VaryImage.vue'
+
+export default {
+  components: {
+    VaryImage
+  },
+  props: {
+    src: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .thumbnail {
+    width: 100%;
+
+    .vary-image {
+      height: auto;
+    }
+  }
+</style>
