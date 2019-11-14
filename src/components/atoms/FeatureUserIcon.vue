be670557e50c641fcb1b09b90aeaee0996192467
Setting6 <thetakahito@gmail.com>
2019-11-14 23:51:27 +0900

    Add: create atoms to FeatureUserIcon

---
+++ src/components/atoms/FeatureUserIcon.vue
@@ -0,0 +1,28 @@
+<template lang="pug" functional>
+  .feature-user-icon(
+    :style="{ backgroundImage: `url(${props.url})` }"
+    )
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .feature-user-icon {
+    width: 70px;
+    height: 70px;
+    border: 3px solid #fff;
+    border-radius: 50%;
+    background-size: cover;
+    background-color: #dcdcdc;
+    box-sizing: border-box;
+  }
+</style>
