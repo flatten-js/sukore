01397741628566355e6241d860b87de5901e2759
Setting6 <thetakahito@gmail.com>
2019-11-14 23:52:54 +0900

    Add: create atoms to MultiLineHeading

---
+++ src/components/atoms/MultiLineHeading.vue
@@ -0,0 +1,22 @@
+<template lang="pug" functional>
+  .multi-line-heading {{ props.text }}
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .multi-line-heading {
+    font-size: 1.125rem;
+    font-weight: bold;
+    color: #1a1a1a;
+  }
+</style>
