469139c94b36fffb658e3e04c7c12f67f2ffe9be
Setting6 <thetakahito@gmail.com>
2019-11-14 23:46:52 +0900

    Add: create atoms to Masthead

---
+++ src/components/atoms/Masthead.vue
@@ -0,0 +1,26 @@
+<template lang="pug" functional>
+  .masthead(
+    :style="{ backgroundImage: `url(${props.url})` }"
+    )
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .masthead {
+    width: 100%;
+    height: 125px;
+    background-size: contain;
+    background-attachment: fixed;
+    background-color: #d1d1d1;
+  }
+</style>
