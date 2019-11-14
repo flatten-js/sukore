508e0ef00726058de8301d008bcdb0a693a4f684
Setting6 <thetakahito@gmail.com>
2019-11-14 23:55:54 +0900

    Add: create atoms to MultiLineText

---
+++ src/components/atoms/MultiLineText.vue
@@ -0,0 +1,14 @@
+<template lang="pug" functional>
+  .multi-line-text {{ props.text }}
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
