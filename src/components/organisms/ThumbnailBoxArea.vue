e75489ef74572dd697ccaaa5c449937065ee708c
Setting6 <thetakahito@gmail.com>
2019-11-16 01:44:48 +0900

    Add: create organisms to ThumbnailBoxArea

---
+++ src/components/organisms/ThumbnailBoxArea.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  section.thumbnail-box-area
+    .thumbnail-box-area__navigation
+      slot(name="navigation")
+    .thumbnail-box-area__contents
+      slot(name="contents")
+</template>
+
+<style lang="scss" scoped>
+  .thumbnail-box-area {
+    &__navigation {
+      border-bottom: .5px solid rgba(26, 26, 26, .2);
+    }
+  }
+</style>
