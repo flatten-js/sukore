22a1f9c6f74098e951b0928f86cebe4e53be3ba1
Setting6 <thetakahito@gmail.com>
2019-11-16 01:40:51 +0900

    Add: create molecules to FlexTabBar

---
+++ src/components/molecules/FlexTabBar.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  nav.flex-tab-bar
+    template(v-for="tab in tabItems")
+      flex-tab-item(
+        :to="tab.to"
+        :text="tab.text"
+        :match="match"
+        )
+</template>
+
+<script>
+import FlexTabItem from '@/components/atoms/FlexTabItem.vue'
+
+export default {
+  components: {
+    FlexTabItem
+  },
+  props: {
+    tabItems: {
+      type: Array,
+      required: true
+    },
+    match: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .flex-tab-bar {
+    display: flex;
+  }
+</style>
