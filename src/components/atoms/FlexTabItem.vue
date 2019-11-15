0482b5170dd8513f07d26ca8601839f39a903aa8
Setting6 <thetakahito@gmail.com>
2019-11-16 01:39:24 +0900

    Add: create atoms to FlexTabItem

---
+++ src/components/atoms/FlexTabItem.vue
@@ -0,0 +1,63 @@
+<template lang="pug">
+  router-link.flex-tab-item(
+    :class="propsClassGenerator"
+    :to="to"
+    active-class="-active"
+    exact-active-class="-exact-active"
+    exact
+    )
+    | {{ text }}
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    match: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    propsClassGenerator() {
+      const { match } = this
+
+      return {
+        ['-match']: match
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .flex-tab-item {
+    padding: 1rem;
+    font-size: .875rem;
+    font-weight: bold;
+    text-align: center;
+    text-decoration: none;
+    color: #1a1a1a;
+
+    &.-exact-active {
+      color: rgba(26, 26, 26, 1);
+      border-bottom: 3px solid #1a1a1a;
+    }
+
+    &:not(.-exact-active) {
+      color: rgba(26, 26, 26, .5);
+      border-bottom: 3px solid transparent;
+    }
+
+    &.-match {
+      flex: 1 1 auto;
+    }
+  }
+</style>
