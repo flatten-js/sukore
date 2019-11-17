c21e3e130fcb71e3a924315e7fccde4808fc06a9
Setting6 <thetakahito@gmail.com>
2019-11-17 18:49:08 +0900

    Add: create molecules to UserDetailsBar

---
+++ src/components/molecules/UserDetailsBar.vue
@@ -0,0 +1,75 @@
+<template lang="pug">
+  .user-details-bar
+    .user-details-bar__icon
+      user-icon(
+        size="small"
+        :url="icon"
+        )
+    .user-details-bar__name
+      single-line-text(
+        :text="name"
+        size="small"
+        weight="bold"
+        )
+    .user-details-bar__button
+      anchor-button(
+        :href="screenName | convertTwitterPath"
+        text="Twitterを見る"
+        )
+</template>
+
+<script>
+import UserIcon from '@/components/atoms/UserIcon.vue'
+import SingleLineText from '@/components/atoms/SingleLineText.vue'
+import AnchorButton from '@/components/atoms/AnchorButton.vue'
+
+export default {
+  components: {
+    UserIcon,
+    SingleLineText,
+    AnchorButton
+  },
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    screenName: {
+      type: String,
+      required: true
+    }
+  },
+  filters: {
+    convertTwitterPath(screenName) {
+      return `https://twitter.com/${screenName}`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .user-details-bar {
+    display: flex;
+    padding: .5rem 1rem;
+    align-items: center;
+    box-sizing: border-box;
+
+    &__icon {
+      margin-right: .5rem;
+    }
+
+    &__name {
+      margin-right: 1rem;
+      flex: 1 1 100%;
+      overflow: hidden;
+    }
+
+    &__button {
+      word-break: keep-all;
+    }
+  }
+</style>
