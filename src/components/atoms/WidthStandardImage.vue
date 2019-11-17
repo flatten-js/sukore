7c611a819840804d25614c8c9c8d1b3048a80264
Setting6 <thetakahito@gmail.com>
2019-11-17 22:40:43 +0900

    Add: create atoms to WidthStandardImage

---
+++ src/components/atoms/WidthStandardImage.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  v-lazy-image.width-standard-image(
+    :src="src"
+    )
+</template>
+
+<script>
+import VLazyImage from 'v-lazy-image'
+
+export default {
+  components: {
+    VLazyImage
+  },
+  props: {
+    src: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .width-standard-image {
+    width: 100%;
+    height: auto;
+    background-color: #dcdcdc;
+  }
+</style>
