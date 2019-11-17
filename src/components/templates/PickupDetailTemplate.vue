10f93c7bc927f4b7955753a3c96e235d74f10946
Setting6 <thetakahito@gmail.com>
2019-11-17 22:51:38 +0900

    Improve: Structural changes due to major component changes

--- src/components/templates/PickupDetailTemplate.vue
+++ src/components/templates/PickupDetailTemplate.vue
@@ -1,13 +1,5 @@
 <template lang="pug">
   .pickup-detail-template
-    section.user-detail-bar-section
-      slot(name="user-detail-bar")
-    section.thumbnail-detail-section
-      slot(name="thumbnail-detail")
+    .pickup-detail-template__image-details-card
+      slot(name="image-details-card")
 </template>
-
-<style scoped>
-  .user-detail-bar-section, .thumbnail-detail-section {
-    border-bottom: .5px solid rgba(26, 26, 26, .2);
-  }
-</style>
