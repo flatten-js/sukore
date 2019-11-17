86614ca2abf3a4eeb94be4ad64b850eeb5805fcd
Setting6 <thetakahito@gmail.com>
2019-11-17 17:14:20 +0900

    Improve: Significant changes with the renewal of the atoms component

--- src/components/molecules/ThumbnailBox.vue
+++ src/components/molecules/ThumbnailBox.vue
@@ -1,27 +1,21 @@
 <template lang="pug">
-  div.thumbnail-box(
+  .thumbnail-box(
     ref="thumbnailBox"
     :class="propsClassGenerator"
     )
-    router-link.thumbnail-box-link(:to="id | pathConversion(screenName)")
-      vary-image(
-        :src="src"
-        fit="cover"
-        @load="load"
-        )
-      template(v-if="size > 1")
-        .thumbnail-size-badge
-          .badge-icon
-            material-icons(
-              name="photo_library"
-              size="custom"
-              )
-          inline-part(
-            :part="size"
-            color="white"
-            )
-      .thumbnail-fav(
-        :class="{ favorite: state }"
+    router-link.thumbnail-box-link(
+      :to="id | convertDetailsPath(screenName)"
+      )
+      .thumbnail-box-link__thumbnail
+        box-image(
+          :src="src"
+          @loadingComplete="loadingComplete"
+          )
+      .thumbnail-box-link__badge
+        template(v-if="imageCount > 1")
+            image-count-badge(:number="imageCount")
+      .thumbnail-box-link__fav(
+        :class="{ '-favorite': state }"
         @click.prevent="clickFavorite"
         )
         material-icons(
@@ -31,75 +25,67 @@
 </template>
 
 <script>
-import VaryImage from '@/components/atoms/VaryImage.vue'
-import InlinePart from '@/components/atoms/InlinePart.vue'
+import BoxImage from '@/components/atoms/BoxImage.vue'
+import ImageCountBadge from '@/components/atoms/ImageCountBadge.vue'
 import MaterialIcons from '@/components/atoms/MaterialIcons.vue'
 
 export default {
   components: {
-    VaryImage,
-    InlinePart,
+    BoxImage,
+    ImageCountBadge,
     MaterialIcons
   },
   props: {
     id: {
       type: String,
-      reqired: true
-    },
-    src: {
-      type: String,
-      reqired: true
+      required: true
     },
     screenName: {
       type: String,
       required: true
     },
-    size: {
+    src: {
+      type: String,
+      required: true
+    },
+    imageCount: {
       type: Number,
-      reqired: true
+      required: true
     },
     state: {
       type: Boolean,
-      reqired: true
-    },
-    column: {
-      type: String,
-      default: '2',
-      validator(val) {
-        return ['2', '3'].includes(val)
-      }
+      required: true
     },
     animation: {
       type: [String, null],
       default: null,
       validator(val) {
-        return ['fade', 'fade-up'].includes(val)
+        return ['fade'].includes(val)
       }
     }
   },
   filters: {
-    pathConversion(val, screenName) {
-      return `/${screenName}/media/${val}`
+    convertDetailsPath(id, screenName) {
+      return `/${screenName}/media/${id}`
     }
   },
   computed: {
     propsClassGenerator() {
-      const { column, animation } = this
+      const { animation } = this
 
       return {
-        [`column-${column}`]: column,
-        [`animation-${animation}`]: animation
+        [`-animation-${animation}`]: animation
       }
     }
   },
   methods: {
-    load: function() {
+    loadingComplete() {
       if (!this.animation) return
 
       const { thumbnailBox } = this.$refs
-      thumbnailBox.classList.add('thumbnail-loaded')
+      thumbnailBox.classList.add('-loading-complete')
     },
-    clickFavorite: function() {
+    clickFavorite() {
       this.$emit('clickFavorite', this.$refs.thumbnailBox)
     }
   }
@@ -110,74 +96,42 @@ export default {
   .thumbnail-box {
     position: relative;
 
-    &.column-2 {
-      width: 50vw;
-      height: 50vw;
-    }
-
-    &.column-3 {
-      width: 33.33vw;
-      height: 33.33vw;
-    }
-
-    &.animation-fade {
+    &.-animation-fade {
       opacity: 0;
       transition: all ease-out .4s;
     }
 
-    &.animation-fade-up {
-      opacity: 0;
-      transform: translate3d(0, 1rem, 0);
-      transition: all ease-out .4s;
-    }
-  }
-
-  .thumbnail-loaded {
-    &.animation-fade {
-      opacity: 1;
-    }
-
-    &.animation-fade-up {
-      opacity: 1;
-      transform: translate3d(0, 0, 0);
+    &.-loading-complete {
+      &.-animation-fade {
+        opacity: 1;
+      }
     }
   }
 
   .thumbnail-box-link {
     display: block;
     height: 100%;
-  }
-
-  .thumbnail-size-badge {
-    display: flex;
-    position: absolute;
-    padding: .25rem .5rem;
-    top: .5rem;
-    right: .5rem;
-    color: #fff;
-    border-radius: 25px;
-    background-color: rgba(26, 26, 26, .75);
-    align-items: center;
-    font-size: .75rem;
-  }
-
-  .badge-icon {
-    margin: 0 .25rem 0 0;
     line-height: 0;
-  }
 
-  .thumbnail-fav {
-    position: absolute;
-    padding: .5rem;
-    bottom: 0;
-    right: 0;
-    color: rgba(255, 255, 255, .95);
-    font-size: 1.8rem;
-    line-height: 0;
-    text-shadow: 0 2px 5px rgba(26, 26, 26, .26);
+    &__badge {
+      position: absolute;
+      top: .5rem;
+      right: .5rem;
+    }
 
-    &.favorite {
-      color: #FF4063;
+    &__fav {
+      position: absolute;
+      padding: .5rem;
+      bottom: 0;
+      right: 0;
+      color: rgba(255, 255, 255, .95);
+      font-size: 1.8rem;
+      line-height: 0;
+      text-shadow: 0 2px 5px rgba(26, 26, 26, .26);
+
+      &.-favorite {
+        color: #FF4063;
+      }
     }
   }
 </style>
