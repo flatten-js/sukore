6375b0b54f8c572bdc8379ce56a03883a5c69634
Setting6 <thetakahito@gmail.com>
2019-11-17 22:47:36 +0900

    Add: create organisms to ImageDetailsCard

---
+++ src/components/organisms/ImageDetailsCard.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  section.image-details-card
+    .image-details-card__head
+      slot(name="head")
+    .image-details-card__body
+      slot(name="body")
+</template>
+
+<style lang="scss" scoped>
+  .image-details-card {
+    %common-style {
+      border-bottom: 1px solid rgba(26, 26, 26, .2);
+    }
+
+    &__head {
+      @extend %common-style;
+    }
+
+    &__body {
+      @extend %common-style;
+    }
+  }
+</style>
