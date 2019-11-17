d88eb97ffaf8fc406663bb63296649817967590a
Setting6 <thetakahito@gmail.com>
2019-11-17 16:49:16 +0900

    Add: create atoms to ImageCountBadge

---
+++ src/components/atoms/ImageCountBadge.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  .image-count-badge
+    .image-count-badge__icon
+      i.material-icons photo_library
+    .image-count-badge__text
+      | {{ number }}
+</template>
+
+<script>
+export default {
+  props: {
+    number: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .image-count-badge {
+    display: flex;
+    padding: .25rem .5rem;
+    border-radius: 25px;
+    background-color: rgba(26, 26, 26, .75);
+    align-items: center;
+    font-size: .75rem;
+    color: #fff;
+
+    &__icon {
+      margin: 0 .25rem 0 0;
+      line-height: 0;
+    }
+  }
+
+  .material-icons {
+    font-size: inherit;
+  }
+</style>
