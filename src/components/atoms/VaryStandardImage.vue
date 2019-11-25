8e435bbfec066e5b296944f8edc6a5fda042ca99
Setting6 <thetakahito@gmail.com>
2019-11-25 16:13:06 +0900

    Add: create atoms to VaryStandardImage with standard as an option

---
+++ src/components/atoms/VaryStandardImage.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+  v-lazy-image.vary-standard-image(
+    :class="propsClassGenerator"
+    :src="src"
+    )
+</template>
+
+<script>
+import VLazyImage from 'v-lazy-image'
+
+export default {
+  components: {
+    VLazyImage
+  },
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    standard: {
+      type: String,
+      default: 'width',
+      validator(val) {
+        return ['width', 'height'].includes(val)
+      }
+    }
+  },
+  computed: {
+    propsClassGenerator() {
+      const { standard } = this
+
+      return {
+        [`-standard-${standard}`]: standard
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .vary-standard-image {
+    &.-standard-width {
+      width: 100%;
+      height: auto;
+    }
+
+    &.-standard-height {
+      width: auto;
+      height: 100%;
+    }
+  }
+</style>
