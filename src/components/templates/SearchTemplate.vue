379ca03db3b26e3c0d67d457afb29bfd89c27ad0
Setting6 <thetakahito@gmail.com>
2019-12-01 16:37:47 +0900

    Add: create templates to SearchTemplate

---
+++ src/components/templates/SearchTemplate.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  .search-template
+    .search-template__masthead
+      slot(name="mastehead")
+    .search-template__thumbnail-box-area
+      slot(name="thumbnail-box-area")
+</template>
