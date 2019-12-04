44e7fe614b811c30237c5dd1d4ee373c315c76ed
Setting6 <thetakahito@gmail.com>
2019-12-04 15:37:21 +0900

    Add: Base completion

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,2 +1,145 @@
 <template lang="pug">
+  search-template
+    thumbnail-area
+      template(#masthead)
+        //- ToDo: ifを使わず初期化処理
+        template(v-if="searchResult.masthead")
+          thumbnail-masthead(
+            :url="searchResult.masthead.src[0]"
+            :id="searchResult.masthead.id"
+            :query="query"
+            :text="searchResult.masthead.text"
+            :image-count="searchResult.masthead.size"
+            :state="searchResult.masthead.state"
+            @clickFavorite="clickFavorite"
+            )
+      template(#contents)
+        thumbnail-box-grid
+          template(v-for="media in searchResult.body")
+            thumbnail-box(
+              ref="calleeThumbnailBox"
+              :key="media.id"
+              :id="media.id"
+              :src="media.src[0]"
+              :screenName="media.screenName"
+              :image-count="media.size"
+              :state="media.state"
+              animation="fade"
+              @clickFavorite="clickFavorite"
+              )
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+import { FAVORITE } from '@/constants/graphql'
+
+import SearchTemplate from '@/components/templates/SearchTemplate.vue'
+import ThumbnailArea from '@/components/organisms/ThumbnailArea.vue'
+import ThumbnailMasthead from '@/components/molecules/ThumbnailMasthead.vue'
+import ThumbnailBoxGrid from '@/components/organisms/ThumbnailBoxGrid.vue'
+import ThumbnailBox from '@/components/molecules/ThumbnailBox.vue'
+
+export default {
+  components: {
+    SearchTemplate,
+    ThumbnailArea,
+    ThumbnailMasthead,
+    ThumbnailBoxGrid,
+    ThumbnailBox
+  },
+  props: {
+    query: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      favorites: []
+    }
+  },
+  apollo: {
+    favorites: FAVORITE.ALL
+  },
+  computed: {
+    ...mapGetters([
+      'noMediaListDuplicate'
+    ]),
+    searchResult() {
+      const [masthead, ...body] = this.noMediaListDuplicate
+
+      return {
+        masthead,
+        body
+      }
+    }
+  },
+  async mounted() {
+    await this.tweetsSearch(this.query, 'popular', 100)
+    await this.$store.commit('initMediaListState', { favorites: this.favorites })
+  },
+  methods: {
+    async tweetsSearch(query, resultType, count) {
+      await this.$store.dispatch('tweetsSearch', {
+        query,
+        resultType,
+        count
+      })
+    },
+    clickFavorite(id) {
+      const media = this.noMediaListDuplicate.find(media => media.id === id)
+
+      this.$store.commit('updateMediaListState', { tid: media.id })
+
+      if (media.state) {
+        this.$apollo.mutate({
+          mutation: FAVORITE.REMOVE,
+          variables: {
+            tid: media.id
+          },
+          update: (store, { data: { deleteManyFavorites } }) => {
+            const data = store.readQuery({ query: FAVORITE.ALL })
+            data.favorites = data.favorites.filter(fav => fav.tid !== media.id)
+            store.writeQuery({ query: FAVORITE.ALL, data })
+          },
+          optimisticResponse: {
+            __typename: 'Mutation',
+            deleteManyFavorites: {
+              __typename: 'Favorite',
+              count: 0
+            }
+          }
+        })
+        .catch(err => {
+          this.$store.commit('updateMediaListState', { tid: media.id })
+        })
+      } else {
+        this.$apollo.mutate({
+          mutation: FAVORITE.ADD,
+          variables: {
+            tid: media.id
+          },
+          update: (store, { data: { createFavorite } }) => {
+            const data = store.readQuery({ query: FAVORITE.ALL })
+            data.favorites.push({
+              __typename: createFavorite.__typename,
+              tid: createFavorite.tid
+            })
+            store.writeQuery({ query: FAVORITE.ALL, data })
+          },
+          optimisticResponse: {
+            __typename: 'Mutation',
+            createFavorite: {
+              __typename: 'Favorite',
+              tid: media.id
+            }
+          }
+        })
+        .catch(err => {
+          this.$store.commit('updateMediaListState', { tid: media.id })
+        })
+      }
+    }
+  }
+}
+</script>
