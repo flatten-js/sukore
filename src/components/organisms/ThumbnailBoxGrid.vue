08f89f32a2d80359c1743addfe33e4b1e74d46ab
Setting6 <thetakahito@gmail.com>
2019-12-21 15:59:12 +0900

    Add: Set a border in the thumbnail box for each column

--- src/components/organisms/ThumbnailBoxGrid.vue
+++ src/components/organisms/ThumbnailBoxGrid.vue
@@ -3,10 +3,36 @@
     slot
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
   .thumbnail-box-grid {
     display: flex;
     flex-wrap: wrap;
     align-items: center;
+
+    .thumbnail-box {
+      &.-column-2 {
+        border-bottom: 4px solid white;
+        box-sizing: border-box;
+
+        &:nth-child(-n+2) {
+          border-top: 4px solid white;
+        }
+
+        &:nth-child(odd) {
+          border-left: 4px solid white;
+          border-right: 2px solid white;
+        }
+
+        &:nth-child(even) {
+          border-left: 2px solid white;
+          border-right: 4px solid white;
+        }
+
+        &:nth-child(odd):last-child {
+          width: calc(50% + 2px);
+          border-right: 4px solid white;
+        }
+      }
+    }
   }
 </style>
--- src/views/Search/index.vue
+++ src/views/Search/index.vue
@@ -22,6 +22,7 @@
               :src="media.src[0]"
               :screenName="media.screenName"
               :image-count="media.size"
+              :thumbnail-size="media.thumbnailSize"
               :state="media.state"
               @clickFavorite="clickFavorite"
               )
