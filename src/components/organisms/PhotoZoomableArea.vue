4f91b52b06273b63b3336f3cdca656fef1e7d2e6
Setting6 <thetakahito@gmail.com>
2020-01-02 21:45:19 +0900

    Add: Creating the framework for PhotoZoomableContent

---
+++ src/components/organisms/PhotoZoomableArea.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  .photo-zoomable-area
+    .photo-zoomable-area__return
+      slot(name="return")
+    .photo-zoomable-area__photo
+      slot(name="photo")
+    .photo-zoomable-area__zoom-controls
+      slot(name="controls")
+</template>
+
+<style lang="scss" scoped>
+  .photo-zoomable-area {
+    position: relative;
+
+    &__return {
+      position: absolute;
+      top: 1rem;
+      left: 1rem;
+    }
+
+    &__zoom-controls {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+    }
+  }
+</style>
--- src/views/PickupDetail/children/PhotoZoomableContent.vue
+++ src/views/PickupDetail/children/PhotoZoomableContent.vue
@@ -1,8 +1,17 @@
 <template lang="pug">
+  photo-zoomable-area
+    template(#return)
+    template(#photo)
+    template(#controls)
 </template>
 
 <script>
+import PhotoZoomableArea from '@/components/organisms/PhotoZoomableArea.vue'
+
 export default {
+  components: {
+    PhotoZoomableArea
+  },
   props: {
     id: {
       type: String,
