78088bb7a3e26f56991cc792d99144410724c4ce
saito-Setting6 <thetakahito@gmail.com>
2020-01-28 21:49:26 +0900

    Rename: GRAPHQL to GRAPHCMS

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -6,7 +6,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
 
 const httpLink = new HttpLink({
-  uri: process.env.VUE_APP_GRAPHQL_ENDPOINT
+  uri: process.env.VUE_APP_GRAPHCMS_ENDPOINT
 })
 
 const apolloClient = new ApolloClient({
