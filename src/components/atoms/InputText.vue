b93ede65bea3228085bf0e95a4aef777668805e1
saito-Setting6 <thetakahito@gmail.com>
2020-02-04 23:33:39 +0900

    Add: Component making to become the base of input

---
+++ src/components/atoms/InputText.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+  input.input-text(
+    :type="type"
+    :placeholder="placeholder"
+    v-model="innerInputText"
+    )
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'inputText',
+    event: 'input'
+  },
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+      validator(val) {
+        return ['text'].includes(val)
+      }
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    inputText: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    innerInputText: {
+      get() {
+        return this.inputText
+      },
+      set(val) {
+        this.$emit('input', val)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .input-text {
+    display: inline;
+    width: 100%;
+    padding: .5rem;
+    font-size: .9rem;
+    border: none;
+    background-color: transparent;
+    outline: 0;
+    box-sizing: border-box;
+  }
+</style>
