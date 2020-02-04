7c0f01677df8e52a7eb7ad720a3167d961d1dabf
saito-Setting6 <thetakahito@gmail.com>
2020-02-04 23:29:46 +0900

    Add: Layout template making of the Explore page

---
+++ src/components/templates/ExploreTemplate.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  .explore-template
+    .explore-template__search-area
+      slot(name="search-area")
+</template>
+
+<style lang="scss" scoped>
+  .explore-template {
+    background-color: white;
+    
+    &__search-area {
+      padding: 1rem;
+    }
+  }
+</style>
