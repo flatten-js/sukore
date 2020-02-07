f53e892be75d57a8eab1df115f9b8953b3136bfc
saito-Setting6 <thetakahito@gmail.com>
2020-02-07 11:03:42 +0900

    Improve: I change the name of the timeline lap component and add a loading frame

---
+++ src/components/organisms/TimelineArea.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  .timeline-area
+    .timeline-area__loading
+      slot(name="loading")
+    .timeline-area__stream
+      slot(name="stream")
+</template>
--- src/components/organisms/TimelineStream.vue
+++
@@ -1,4 +0,0 @@
-<template lang="pug">
-  .timeline-stream
-    slot
-</template>
