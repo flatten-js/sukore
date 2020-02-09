e8a879286975454c88c4fe6ad8a6ece203a858c6
saito-Setting6 <thetakahito@gmail.com>
2020-02-10 00:33:32 +0900

    Add: Securing of layout position of the header area

--- src/components/templates/UserTemplate.vue
+++ src/components/templates/UserTemplate.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
   .user-template
+    .user-template__header-area
+      slot(name="header-area")
     .user-template__user-details-catch
       slot(name="user-details-catch")
     .user-template__thumbnail-box-area
