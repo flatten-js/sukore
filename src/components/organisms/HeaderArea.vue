26f56fcced361900d8b089b8ff304c1b33ca87ec
saito-Setting6 <thetakahito@gmail.com>
2020-02-09 22:54:19 +0900

    Add: Component making to wrap contents while maintaining the height of the header

---
+++ src/components/organisms/HeaderArea.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  header.header-area
+    .header-area__content
+      slot
+</template>
+
+<style lang="scss" scoped>
+  .header-area {
+    height: 50px;
+
+    &__content {
+      position: fixed;
+      width: 100%;
+      top: 0;
+      background-color: white;
+      border-bottom: 1px solid #dfdfdf;
+      z-index: 10;
+    }
+  }
+</style>
