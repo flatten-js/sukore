8f69d5448289974f437681f76dac836cca92e674
saito-Setting6 <thetakahito@gmail.com>
2020-02-14 21:18:05 +0900

    Add: I make a component to wrap popup contents

---
+++ src/components/organisms/Popup.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  section.popup
+    .popup__content
+      slot(name="content")
+</template>
+
+<style lang="scss" scoped>
+  .popup {
+    position: relative;
+    height: 100vh;
+    background-color: rgba(26, 26, 26, .5);
+
+    &__content {
+      position: absolute;
+      width: 100%;
+      bottom: 0;
+      left: 0;
+    }
+  }
+</style>
