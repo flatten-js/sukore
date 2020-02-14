f4e29a23daee87c4e64931333b66494cb98112fd
saito-Setting6 <thetakahito@gmail.com>
2020-02-14 21:10:47 +0900

    Add: I add an option area to a layout and secure the frame for the menu option

--- src/components/templates/UserLayout.vue
+++ src/components/templates/UserLayout.vue
@@ -6,4 +6,18 @@
       slot(name="user-details-area")
     .user-layout__thumbnail-box-area
       slot(name="thumbnail-box-area")
+    .user-layout-options-area
+      .user-layout-options-area__menu-popup
+        slot(name="option-menu-popup")
 </template>
+
+<style lang="scss" scoped>
+  .user-layout-options-area {
+    position: fixed;
+    width: 100%;
+    height: auto;
+    top: 0;
+    left: 0;
+    z-index: 9999;
+  }
+</style>
