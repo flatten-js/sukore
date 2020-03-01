3b56009c13a2a15e630d500c3b39ec1946d3fe9d
saito-Setting6 <thetakahito@gmail.com>
2020-03-01 18:03:35 +0900

    Add: I implement loading and a re-search

--- src/components/organisms/ThumbnailArea.vue
+++ src/components/organisms/ThumbnailArea.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
   section.thumbnail-area
+    .thumbnail-area__loading
+      slot(name="loading")
     .thumbnail-area__masthead
       slot(name="masthead")
     .thumbnail-area__contents
--- src/views/Search/index.vue
+++ src/views/Search/index.vue
@@ -7,6 +7,9 @@
           v-model="inputText"
           )
     thumbnail-area
+      template(#loading)
+        template(v-if="loading.likes")
+          absolute-territory
       template(#masthead)
         //- ToDo: ifを使わず初期化処理
         template(v-if="searchMedia.masthead")
@@ -44,6 +47,7 @@ import { shareUpdateLike } from '@/apollo/graphql/used/shares'
 import SearchLayout from '@/components/templates/SearchLayout.vue'
 import HeaderArea from '@/components/organisms/HeaderArea.vue'
 import DetailsBar from '@/components/molecules/DetailsBar.vue'
+import AbsoluteTerritory from '@/components/molecules/AbsoluteTerritory.vue'
 import ThumbnailArea from '@/components/organisms/ThumbnailArea.vue'
 import ThumbnailHead from '@/components/molecules/ThumbnailHead.vue'
 import ThumbnailBoxGrid from '@/components/organisms/ThumbnailBoxGrid.vue'
@@ -54,6 +58,7 @@ export default {
     SearchLayout,
     HeaderArea,
     DetailsBar,
+    AbsoluteTerritory,
     ThumbnailArea,
     ThumbnailHead,
     ThumbnailBoxGrid,
@@ -70,6 +75,9 @@ export default {
       init: {
         likes: false
       },
+      loading: {
+        likes: true
+      },
       likes: [],
       inputText: ''
     }
@@ -85,13 +93,14 @@ export default {
       skip() {
         return this.init.likes
       },
-      result({ data }, key) {
-        this.init = { ...this.init, [key]: true }
-        this.initSearchMediaData({
+      async result({ data }, key) {
+        this.queryInitReady(key)
+        await this.initSearchMediaData({
           query: this.query,
           count: 100,
           likes: data.likes
         })
+        this.queryLoadingReady(key)
       }
     }
   },
@@ -109,6 +118,20 @@ export default {
       }
     }
   },
+  watch: {
+    async '$route.params.query'(to, from) {
+      if (to === from) return
+
+      this.queryLoadingReady('likes')
+      this.$store.commit('initMedia')
+      await this.initSearchMediaData({
+        query: to,
+        count: 100,
+        likes: this.likes
+      })
+      this.queryLoadingReady('likes')
+    }
+  },
   created() {
     this.inputText = this.query
     this.$store.commit('initMedia')
@@ -120,6 +143,12 @@ export default {
     async initSearchMediaData({ type, query, count, likes }) {
       await this.$store.dispatch('multiTweetSearch', { type, query, count })
       await this.$store.commit('initMediaListState', { likes })
+    },
+    queryInitReady(key) {
+      this.init = { [key]: !this.init[key] }
+    },
+    queryLoadingReady(key) {
+      this.loading = { [key]: !this.loading[key] }
     },
     async swaipToRefresh() {
       const el = this.$el,
