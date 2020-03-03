cc1aaa29638eb71479530240406641c5dcadd054
saito-Setting6 <thetakahito@gmail.com>
2020-03-03 13:21:18 +0900

    Add: I secure a frame for the suggest in a layout

--- src/components/templates/ExploreLayout.vue
+++ src/components/templates/ExploreLayout.vue
@@ -1,11 +1,28 @@
 <template lang="pug">
-  .explore-template
-    .explore-template__header-area
+  .explore-layout
+    .explore-layout__header-area
       slot(name="header-area")
+    .explore-layout__suggest-area
+      slot(name="suggest-area")
 </template>
 
 <style lang="scss" scoped>
-  .explore-template {
-    background-color: white;
+  .explore-layout {
+    $header-height: 3.25rem;
+    position: relative;
+
+    &__header-area {
+      height: $header-height;
+    }
+
+    &__suggest-area {
+      position: absolute;
+      width: 100%;
+      top: $header-height;
+      left: 0;
+      max-height: calc(100vh - #{$header-height});
+      overflow-x: hidden;
+      overflow-y: scroll;
+    }
   }
 </style>
