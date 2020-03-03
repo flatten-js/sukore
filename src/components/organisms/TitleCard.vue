6a71f9e9ee9d6f0478fcae80492f3772d5668797
saito-Setting6 <thetakahito@gmail.com>
2020-03-03 13:08:52 +0900

    Add: I make the component which wraps a title and contents, and is unified

---
+++ src/components/organisms/TitleCard.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  section.title-card
+    .title-card__title
+      slot(name="title")
+    .title-card__content
+      slot(name="content")
+</template>
+
+<style lang="scss" scoped>
+  .title-card {
+    background-color: white;
+    border-bottom: 1px solid #dfdfdf;
+  }
+</style>
