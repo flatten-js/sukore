b9e414631f074819ed0f60ce645a809576d70db1
flatten-js <thetakahito@gmail.com>
2020-10-19 21:21:00 +0900

    Add: Create components for global navigation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,42 @@
 <template lang="pug">
   #app
     router-view
+    foot-area
+      navigation(
+        :items="navs"
+        size="large"
+        )
 </template>
+
+<script>
+import FootArea from '@/components/organisms/FootArea.vue'
+import Navigation from '@/components/molecules/Navigation.vue'
+
+export default {
+  components: {
+    FootArea,
+    Navigation
+  },
+  data() {
+    return {
+      navs: [
+        {
+          to: '/',
+          name: 'home'
+        },
+        {
+          to: '/explore',
+          name: 'search'
+        },
+        {
+          to: '/i/likes',
+          name: 'like'
+        }
+      ]
+    }
+  }
+}
+</script>
 
 <style scoped>
   #app {
---
+++ src/components/molecules/Navigation.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  nav.navigation
+    template(v-for="item in items")
+      router-link.navigation__tab(
+        :to="item.to"
+        active-class="-active"
+        exact-active-class="-exact-active"
+        )
+        svg-sprite(
+          :name="item.name"
+          size="large"
+          )
+</template>
+
+<script>
+import SvgSprite from '@/components/atoms/SvgSprite.vue'
+
+export default {
+  components: {
+    SvgSprite
+  },
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.navigation {
+  display: flex;
+  justify-content: center;
+
+  &__tab {
+    flex-grow: 1;
+    padding: .75rem;
+    text-align: center;
+    color: rgba(26, 26, 26, 0.5);
+
+    &.-exact-active {
+      color: #1DA1F2;
+    }
+  }
+}
+</style>
